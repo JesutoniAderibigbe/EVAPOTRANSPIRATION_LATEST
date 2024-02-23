1a6b99775127054d75f9afe0863801a01e1b17db
Jesutoni Aderibigbe <user@MACs-MacBook-Air-2.local>
2024-02-23 08:44:38 +0100

    added firebase for firebase distribution

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/scheduler.dart';
 import 'core/app_export.dart';
+import 'firebase_options.dart';
 
 var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+
+  //add firebase
+  await Firebase.initializeApp(
+     options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   ///Please update theme as per your need if required.
   ThemeHelper().changeTheme('primary');
